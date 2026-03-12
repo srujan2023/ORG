f96f9bc838e88972c935706e23ff99599c865f8c
srujan <srujan2004ab@gmail.com>
2026-03-12 16:02:42 +0530

    Header files Pages Created

--- creative/web/urls.py
+++ creative/web/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("services/", views.services, name="services"),
+    path("career/", views.career, name="career"),
+    path("gallery/", views.gallery, name="gallery"),
+    path("contact/", views.contact, name="contact"),
 ]
-
--- creative/web/views.py
+++ creative/web/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def services(request):
+    return render(request, "services.html")
+
+
+def career(request):
+    return render(request, "career.html")
+
+
+def gallery(request):
+    return render(request, "gallery.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
